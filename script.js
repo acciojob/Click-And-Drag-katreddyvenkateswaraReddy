c4f192a318de28b2b5d905f84d16cf01fce69216
katreddyvenkateswaraReddy <63310358+katreddyvenkateswaraReddy@users.noreply.github.com>
2023-12-22 00:47:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Your code here.
+
+const item = document.getElementsByClassName("item");
+const items = document.getElementsByClassName("items");
+
+for (let i = 0; i < items.length; i++) {
+	item[i].addEventListener("dragstart", (event) => {
+		event.dataTransfer.setData("id", item[i].id);
+	});
+
+	item[i].addEventListener("dragover", (event) => {
+  event.preventDefault();
+});
+}
+
+
+
+items.addEventListener("drop", (event) => {
+  const droppedElementId = event.dataTransfer.getData("id");
+  const element = document.getElementById(droppedElementId); //
+  element.draggable = false;
+  items.appendChild(element);
+});
